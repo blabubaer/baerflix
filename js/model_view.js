782f3c6bbc045cd6be24111613ca37314bd09973
Marten <mgransow@gmail.com>
2021-03-02 12:08:01 +0100

    set up started

---
+++ js/model_view.js
@@ -0,0 +1,12 @@
+// Model
+var mmodel = {
+    page: 0, // 0=Home, 1=details, 2=new-movie
+    current_movie: {}, //Currently chosen movie
+
+}
+
+
+function view(){
+    
+
+}
--- python/indexing.py
+++ python/indexing.py
@@ -1,8 +1,23 @@
 import os
-listoffolder = []
-directory = 'M:/01 Movies/00 Eingang'
-dirlist = os.listdir(directory)
-for d in dirlist:
-    full_path = os.path.join(directory, d)
-    listoffolder.append(full_path)
-print(len(listoffolder))
+import json
+
+todolist = ['M:/01 Movies',]
+filmlist = []
+
+while len(todolist)>0:
+    dirlist = os.listdir(todolist[0])
+    for dir in dirlist:        
+        full_path = os.path.join(todolist[0],dir)
+        if os.path.isdir(full_path):
+            todolist.append(full_path)
+        elif (full_path.endswith(".mkv") or full_path.endswith(".avi")) and (os.stat(full_path).st_size > 200000000):            
+            filmlist.append({
+                'filename' : dir,
+                'folderpath' : todolist[0],
+                'filepath' :full_path,
+            })
+    todolist.pop(0)
+
+
+with open('raw_filmlist.json', 'w') as outfile:
+    json.dump(filmlist, outfile)
